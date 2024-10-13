ed61c3ec46f472190711ec059baed14201b0fe96
Wiches09 <109953465+Wiches09@users.noreply.github.com>
2024-10-14 05:27:06 +0700

    add layout and navbar template

--- bookBooking/booking/views.py
+++ bookBooking/booking/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.db.models import Q
 
 # Create your views here.
-class 
+
+
+class index(View):
+    def get(self, request):
+        return render(request, "test.html")
