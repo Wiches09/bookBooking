9cf9a0c29f67de87c4a42b3f0a25fb787398f5d8
Suchanan239 <65070239@kmitl.ac.th>
2024-10-14 12:34:14 +0700

    Edit booking/views/Index, Add static Folder

--- bookBooking/booking/views.py
+++ bookBooking/booking/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.db.models import Q
+from django.http import HttpRequest
+from .models import *
 
 # Create your views here.
 
 
 class Index(View):
     def get(self, request):
-        return render(request, "index.html")
+        query = request.GET
+        books = Book.objects.all()
+
+        if query.get("search"):
+            book = books.filter(
+                Q(name__name__icontains=query.get("search")) |
+                Q(author__name__icontains=query.get("search")) |
+                Q(author__name__icontains=query.get("search"))
+            )
+
+        return render(request, "index.html", {
+            "books": books
+        })
