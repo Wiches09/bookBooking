de7b4a755b5b555c2740f04135b3426257ffa31e
Wiches09 <109953465+Wiches09@users.noreply.github.com>
2024-10-15 13:48:35 +0700

    add static file (#12)

--- bookBooking/manageBook/urls.py
+++ bookBooking/manageBook/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("manage_index/", views.index.as_view(), name="index")
+    # path("manage_index/", views.Index.as_view(), name="index")
 ]
