e7b7f5b0c61ea40e1de8f42aa69c92716481f69c
Wiches09 <109953465+Wiches09@users.noreply.github.com>
2024-10-15 17:18:03 +0700

    fix

--- bookBooking/manageBook/urls.py
+++ bookBooking/manageBook/urls.py
@@ -2,9 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path("index/", views.BookManagement.as_view(), name="manageBook")
-=======
-    # path("manage_index/", views.Index.as_view(), name="index")
->>>>>>> de7b4a755b5b555c2740f04135b3426257ffa31e
 ]
