19282c728d5e3fa149e485cb8f8854f5c814ee5c
Wiches09 <109953465+Wiches09@users.noreply.github.com>
2024-10-16 01:33:54 +0700

    chages models -> use django

--- bookBooking/booking/models.py
+++ bookBooking/booking/models.py
@@ -1,36 +1,8 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class Account(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.username
-
-
-class Member(models.Model):
-    account = models.OneToOneField(
-        Account, on_delete=models.CASCADE, primary_key=True)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    email = models.EmailField(max_length=255)
-    phone_number = PhoneNumberField()
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
-
-class Staff(models.Model):
-    account = models.OneToOneField(
-        Account, on_delete=models.CASCADE, primary_key=True)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
 
 
 class BookStatus(models.Model):
@@ -41,7 +13,7 @@ class BookStatus(models.Model):
 
 
 class Cart(models.Model):
-    member = models.OneToOneField(Member, on_delete=models.CASCADE)
+    member = models.OneToOneField(User, on_delete=models.CASCADE)
     create_date = models.DateField()
 
     def __str__(self):
@@ -49,7 +21,8 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True, blank=True)
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, null=True, blank=True)
     book = models.ForeignKey('manageBook.Book', on_delete=models.CASCADE)
 
     def __str__(self):
@@ -65,14 +38,14 @@ class BorrowBook(models.Model):
 
 class BorrowHistory(models.Model):
     member = models.ForeignKey(
-        Member, on_delete=models.CASCADE, null=True, blank=True)
+        User, on_delete=models.CASCADE, null=True, blank=True)
     borrow_date = models.DateField()
     return_date = models.DateField(null=True, blank=True)
     book_returned = models.DateField(null=True, blank=True)
     fine = models.DecimalField(decimal_places=2, max_digits=5)
     status = models.ForeignKey(BookStatus, on_delete=models.CASCADE)
     staff = models.ForeignKey(
-        Staff, on_delete=models.CASCADE, null=True, blank=True)
+        User, on_delete=models.CASCADE, null=True, blank=True)
 
     position_in_queue = models.PositiveIntegerField(null=True, blank=True)
     queue_date = models.DateField(null=True, blank=True)
