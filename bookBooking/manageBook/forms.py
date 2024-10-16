5bd2f708d2934a5bab82002e7d1512cfd26803be
Wiches09 <109953465+Wiches09@users.noreply.github.com>
2024-10-17 06:06:30 +0700

    now can add book

--- bookBooking/manageBook/forms.py
+++ bookBooking/manageBook/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from booking.models import BookStatus
 from django.forms import ModelForm
 from manageBook.models import Book
+from django.utils import timezone
 
 
 class SearchBookForm(forms.Form):
@@ -16,8 +17,6 @@ class BookStatusForm(forms.Form):
 
 
 class AddBookForm(ModelForm):
-    
-    
     class Meta:
         model = Book
         fields = [
@@ -28,3 +27,16 @@ class AddBookForm(ModelForm):
             'categories',
             'description',
         ]
+        
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4}),
+            'publish_date': forms.DateInput(attrs={'type': 'date'}),
+        }
+    
+    def clean_publish_date(self):
+        publish_date = self.cleaned_data.get('publish_date')
+        if publish_date and publish_date > timezone.now().date():
+            raise forms.ValidationError("The publish date cannot be in the future.")
+        return publish_date
+
+        
--- bookBooking/manageBook/urls.py
+++ bookBooking/manageBook/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("index/", views.QueueManagementView.as_view(), name="manageBook"),
     path("index/search/", views.QueueManagementView.as_view(), name="searchBook"),
-    path("index/detail/<int:pk>", views.QueueDetailView.as_view(), name="queueDetail")
+    path("index/detail/<int:pk>",
+         views.QueueDetailView.as_view(), name="queueDetail"),
+    path("index/addBook/", views.AddBook.as_view(), name="addBook")
 ]
