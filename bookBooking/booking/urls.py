708bfd389e71b562d9452c0ecd82ec7a36906339
Wiches09 <109953465+Wiches09@users.noreply.github.com>
2024-10-17 00:02:22 +0700

    resolve conflict

--- bookBooking/booking/urls.py
+++ bookBooking/booking/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("index/", views.IndexView.as_view(), name="index"),
     path("cart/", views.CartView.as_view(), name="cart"),
     path("cart/<int:cart_id>/", views.ConfirmBooking.as_view(), name="confirm_booking"),
-    path("index/<int:book_id>", views.BookDetailViewView.as_view(), name="book-detail"),
+    path("index/<int:book_id>", views.BookDetailView.as_view(), name="book-detail"),
     path("history/", views.BorrowHistoryView.as_view(), name="history")
 ]
